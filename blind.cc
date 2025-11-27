7568711cc4db9858087cc49437e0cd4242119d32
Sharuga Suthakaran <sharugasuthakaran@Sharugas-MacBook-Air.local>
2025-11-27 02:29:31 -0500

    blind

--- blind.cc
+++ blind.cc
@@ -1,5 +1,9 @@
 export module Blind;
 
+import Player;
+import Grid;
+import SpecialAction;
+
 export class Blind : public SpecialAction {
     public:
        void apply(Player& p, Grid& g) override;
