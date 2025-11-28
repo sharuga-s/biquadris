fae56a680a051f2da569f8b5ffd1fae1ce4122df
tanisha <tanishasthuti@gmail.com>
2025-11-28 16:09:26 -0500

    small changes

--- display.cc
+++ display.cc
@@ -5,9 +5,9 @@ import Block;
 
 // observer interface - displays observe game state
 export class Display {
-public:
-    virtual ~Display() = default;
-    
-    // observer pattern: called by Subject when state changes
-    virtual void notify() = 0;
+    public:
+        virtual ~Display() = default;
+        
+        // observer pattern: called by Subject when state changes
+        virtual void notify() = 0;
 };
