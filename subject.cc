49a69996a8f7060b07322ca12f1cd78db0310706
tanisha <tanishasthuti@gmail.com>
2025-11-28 19:39:34 -0500

    comments

--- subject.cc
+++ subject.cc
@@ -9,27 +9,28 @@ using namespace std;
 
 // subject (observable) class for Observer pattern
 export class Subject {
-    vector<Display*> observers;
-    
-public:
-    virtual ~Subject() = default;
-    
-    void attach(Display* obs) {
-        observers.push_back(obs);
-    }
-    
-    void detach(Display* obs) {
-        auto it = find(observers.begin(), observers.end(), obs);
-        if (it != observers.end()) {
-            observers.erase(it);
+    protected:
+        vector<Display*> observers;
+
+    public:
+        virtual ~Subject() = default;
+        
+        void attach(Display* obs) {
+            observers.push_back(obs);
         }
-    }
-    
-protected:
-    // called by derived class when state changes
-    void notifyObservers() {
-        for (auto obs : observers) {
-            obs->notify();
+        
+        void detach(Display* obs) {
+            auto it = find(observers.begin(), observers.end(), obs);
+            if (it != observers.end()) {
+                observers.erase(it);
+            }
         }
-    }
+        
+    protected:
+        // called by derived class when state changes
+        void notifyObservers() {
+            for (auto obs : observers) {
+                obs->notify();
+            }
+        }
 };
